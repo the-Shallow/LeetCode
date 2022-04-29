53a053d145af16c7536a7fb88170afafe731ca1e
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-04-29 17:22:04 +0530

    Time: 0 ms (100.00%), Space: 43 MB (89.64%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,5 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        
+         if(nums == null || nums.length == 0){
+            return 0;
+        }
+        
         int res = nums[0];
         
         for(int i = 1;i<nums.length;i++){
