64782ac5c1f58cc0ea4657940aded6fbe649a0ab
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-05-07 18:08:58 +0530

    Time: 55 ms (23.36%), Space: 66.9 MB (46.95%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        int length = nums.length;
+        
+        // for creating the min array
+        int[] min_arr = new int[length];
+        min_arr[0] = nums[0];
+        for(int i = 1;i<length;i++){
+            min_arr[i] = Math.min( min_arr[i-1] , nums[i] );
+        }
+        
+        
+        Stack<Integer> stack = new Stack<>();
+        
+        for( int j = length-1; j>=0 ; j--){
+            int val = nums[j];
+            
+            // values in stack smaller than min values till current j pointer are useless as they are never going to become larger.
+            while( !stack.isEmpty() && stack.peek() <= min_arr[j] ){
+                stack.pop();
+            }
+            
+            // there are still values larger than the minimum at the current j and now we check if the current value is greater than stacks peek value which are nothing but k and current is j.
+            if( !stack.isEmpty()  && stack.peek() < val ) return true;
+            
+            stack.push( val );
+            
+        }
+        
+        // at the very end we simply return false;
+        return false;
+    }
+}
