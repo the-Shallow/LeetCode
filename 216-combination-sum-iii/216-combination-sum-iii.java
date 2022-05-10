80b6d47b3dbdbc3f7199ef17a3482dea07507ca4
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-05-10 16:59:31 +0530

    Time: 1 ms (73.61%), Space: 41.5 MB (60.52%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.java
@@ -0,0 +1,29 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        if( k > n ) return res;
+        
+        for( int i = 1;i<=9;i++ ){
+            helper( i , new ArrayList<Integer>() , k , 0 , n);
+        }
+        
+        return res;
+    }
+    
+    public void helper( int start , List<Integer> list , int max , int curr , int sum ){
+        if( max == 0 ){
+            if( curr == sum && !res.contains(list) ){
+                res.add( new ArrayList<>(list) );
+            }
+            
+            return;
+        }
+        
+        for( int i = start ; i<=9;i++ ){
+            list.add(i);
+            helper( i+1,list,max-1,curr+i,sum );
+            list.remove( list.size() - 1 );
+        } 
+    }
+}
