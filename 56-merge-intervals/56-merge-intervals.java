1264d5d736326885ff36d6c2576a6e10721a4088
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-14 15:34:05 +0530

    Time: 15 ms (35.87%), Space: 54.9 MB (61.46%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,17 +1,14 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if( intervals.length <= 1 ){
-            return intervals;
-        }
+        if( intervals.length <= 1 ) return intervals;
         
-        Arrays.sort(intervals,(arr1,arr2)-> arr1[0] - arr2[0] );
+        Arrays.sort( intervals , (arr1,arr2) -> arr1[0] - arr2[0] );
         
         List<int[]> list = new ArrayList<>();
-        
         int[] curr_interval = intervals[0];
-        list.add(curr_interval);
+        list.add( curr_interval );
         
-        for(int[] interval : intervals){
+        for( int[] interval : intervals ){
             int curr_begin = curr_interval[0];
             int curr_end = curr_interval[1];
             int next_begin = interval[0];
@@ -21,10 +18,10 @@ class Solution {
                 curr_interval[1] = Math.max( curr_end , next_end );
             }else {
                 curr_interval = interval;
-                list.add(interval);
+                list.add(curr_interval);
             }
         }
         
-        return list.toArray(new int[list.size()][]);
+       return list.toArray(new int[list.size()][]);
     }
 }
