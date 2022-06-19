2ef2f98c812ae11b28c5002a5181ab81ff8c36b0
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-19 08:35:12 +0530

    Time: 1 ms (69.69%), Space: 43.3 MB (9.87%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,36 +1,31 @@
+// Space Complexity = O(1)
+// Time Complexity = (logn)
 class Solution {
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
+        int left = 0,right = nums.length-1;
         
         while( left < right ){
-            int midpoint = left + (right - left) / 2;
-            
-            if( nums[midpoint] > nums[right] ) left = midpoint + 1;
-            else right = midpoint; 
+            int mid = left + (right - left) /2;
+            // System.out.println( mid );
+            if( nums[mid] > nums[right] ) left = mid + 1;
+            else right = mid;
         }
         
+        // System.out.println( left + " " + right);
         int start = left;
         left = 0;
         right = nums.length-1;
         
-        if(  target >= nums[start] && target <= nums[right] ){
-            left = start;
-        }else {
-            right = start;
-        }
-        
+        if( target >= nums[start]  && target <= nums[right] ) left = start;
+        else right = start;
         
         while( left <= right ){
-            int midpoint = left + (right - left) / 2;
+            int mid =  left + (right - left) /2;
             
-            if( nums[midpoint] == target ) {
-                return midpoint;
-            }else if( nums[midpoint] <= target ){
-                left = midpoint + 1;
-            }else {
-                right = midpoint - 1;
-            }
+            if( nums[mid] == target ) return mid;
+            
+            if( nums[mid] < target ) left = mid + 1;
+            else right = mid - 1;
         }
         
         return -1;
