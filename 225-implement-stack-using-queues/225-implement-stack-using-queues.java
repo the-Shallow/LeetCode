451096323592afe3d28d2ef6bc6b62a27055314c
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-20 10:55:40 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (20.24%) - LeetHub

---
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -0,0 +1,36 @@
+class MyStack {
+    LinkedList<Integer> intQueue;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        intQueue = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        intQueue.add(x);
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return intQueue.pollLast();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return intQueue.getLast();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return intQueue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
