b81a72a0ce058277c469555bd90126f5b1949b7a
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-23 17:09:59 +0530

    Time: 863 ms (5.01%), Space: 144.7 MB (5.76%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,34 @@
+class Solution {
+    Map<Integer,Integer> map = new HashMap<>();
+    public int lengthOfLIS(int[] nums) {
+        int res = 0;
+        
+        for( int i = 0;i<nums.length;i++ ){
+            int val = helper(nums,nums[i],Integer.MIN_VALUE,i );
+            
+            res = Math.max( res , val );
+        }
+        
+        return res;
+    }
+    
+    public int helper(int[] nums , int curr , int prev , int index ){
+        
+        if( curr <= prev ) return 0;
+        
+        if( index == nums.length ) return 0;
+        
+        if( map.containsKey(index) ) return map.get(index);
+        
+        int count = 0;
+        
+        for( int i = index + 1;i<nums.length;i++ ){
+            int val = nums[i];
+            
+            count = Math.max( count , helper( nums , val , curr , i   ) ) ;
+        }
+        
+        map.put( index , count+1 );
+        return count + 1;
+    }
+}
