54cb73d1235c011813417283711c22019afb73f7
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-27 22:52:50 +0530

    Time: 5 ms (8.88%), Space: 44.9 MB (7.29%) - LeetHub

---
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    class Node {
+        char ch;
+        Node[] children;
+        boolean isEnd;
+        
+        public Node(char ch){
+            this.ch = ch;
+            this.children = new Node[26];
+            this.isEnd = false;
+        }
+    }
+    
+    Node root = new Node('0');
+    public int countPrefixes(String[] words, String s) {
+        int res = 0;
+        insert(s);
+        
+        for( String word : words ){
+            Node curr = root;
+            boolean isWord = true;
+            for(char ch : word.toCharArray()){
+                if( curr.children[ch-'a'] == null ) {
+                    isWord = false;
+                    break;
+                }
+                
+                curr = curr.children[ch-'a'];
+            }
+            
+            if(isWord) res++;
+        }
+        
+        return res;
+    }
+    
+    public void insert( String s ){
+        Node curr = root;
+        
+        for(char ch : s.toCharArray()){
+            if( curr.children[ch-'a'] == null ) curr.children[ch-'a'] = new Node(ch);
+            
+            curr = curr.children[ch-'a'];
+        }
+        
+        curr.isEnd = true;
+    }
+}
