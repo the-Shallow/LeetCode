bdc7511cbb54d03cc583970768815bb2c432c69c
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-27 22:39:51 +0530

    Time: 2476 ms (5.80%), Space: 255.3 MB (5.80%) - LeetHub

--- 1449-form-largest-integer-with-digits-that-add-up-to-target/1449-form-largest-integer-with-digits-that-add-up-to-target.java
+++ 1449-form-largest-integer-with-digits-that-add-up-to-target/1449-form-largest-integer-with-digits-that-add-up-to-target.java
@@ -1,3 +1,5 @@
+// Space Complexity = O(N*M) (N == length of cost array and M == target )
+// Time Complexity = O(N*M)
 class Solution {
     Map<String,String> map =  new HashMap<>();
     String[][] memo;
@@ -43,3 +45,38 @@ class Solution {
         return Double.parseDouble( num1 ) < Double.parseDouble( num2 ) ? num2 : num1;
     }
 }
+
+
+/*
+As we can see we are given a cost array and a target value and we are asked to return the largest string which we can form.
+
+To create the largest string:
+we can take any cost value from index = (0,cost.length-1) + 1 ;(1-based indexing)
+
+If we can not reach to our exact target value(target == 0) we return -1;
+
+
+Approach:
+
+    As we can see this question is quite similar to unbounded knapsack(we can select one digit and again can select from the whole cost array)
+    
+    Two things can happen at each index we either pick the value or not
+    1--> If we pick the cost value then we add that (index+1_) to our curr string and decrment cost[index] value from target and again call helper method fro the whole cost array.
+    2 --> We don't select current value and go index-1;target value remians the same as we have not selected the value.
+    
+    If target < 0(can not be form) return "-1" as there are no negative values.
+    If target == 0 return "".
+    
+    Now At this point we have two string which are retuned.
+    
+    We compare them:
+    if one of the string contains (-1) we return the other string.
+    if one string has length greater than the other we return greater length string
+    At last we compare both the string by  values and return string with greater value;
+    
+    This apprach can take a long amount of time and will give TLE if it is not memorize.
+    So as index and target value are the one changing so we memorize respect to them.
+    
+    To fill the memorize array we use "0" as "0" is not present in the whole cost array.You can also choose "0" as the returned value if target goes less than 0 and "-1" to memorize array.I use it because i feel "-1" gives more meaning but its still the same thing.
+    
+*/
