be56a4d4b42ad169d587f6ed93cd659ca99aa206
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-06-30 10:32:51 +0530

    Time: 15 ms (44.37%), Space: 45.7 MB (34.22%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,13 +1,43 @@
+// Space Complexity = O(n)
+// Time Complexity = O(n)
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         Map<Integer,Integer> map = new HashMap<>();
+        
+//         for( int i = 0; i < nums.length ; i++ ){
+//             if( map.containsKey( target - nums[i] ) ){
+//                 return new int[]{ map.get(target-nums[i]) , i };
+//             }else {
+//                 map.put(nums[i],i);
+//             }
+//         }
+        
+//         return null;
+//     }
+// }
+
+
+// Approach 2
+// Space Complexity = O(N)
+// Time Complexity = O(nlogn)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
         
-        for( int i = 0; i < nums.length ; i++ ){
-            if( map.containsKey( target - nums[i] ) ){
-                return new int[]{ map.get(target-nums[i]) , i };
-            }else {
-                map.put(nums[i],i);
-            }
+        int[][] indexArray = new int[nums.length][2];
+        
+        for(int i = 0;i<nums.length;i++){
+            indexArray[i][0] = nums[i];
+            indexArray[i][1] = i;
+        }
+        
+        Arrays.sort(indexArray,(a,b)->a[0] - b[0]);
+        
+        int start = 0,end = nums.length - 1;
+        while(start < end){
+            int sum = indexArray[start][0] + indexArray[end][0];
+            if(sum == target) return new int[]{indexArray[start][1] , indexArray[end][1]};
+            if( sum > target ) end--;
+            else start++;
         }
         
         return null;
