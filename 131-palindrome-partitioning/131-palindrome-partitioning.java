5f186b98005fb446c4a522163358e5796438b329
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-07-02 09:24:44 +0530

    Time: 38 ms (8.44%), Space: 136.8 MB (66.30%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.java
+++ 131-palindrome-partitioning/131-palindrome-partitioning.java
@@ -1,30 +1,31 @@
-// Space Complexity = O(n*2^n)
-// Time Complexity = O(t*2^n) (t == time to check if it is a palindrome)
 class Solution {
-    List<List<String>> res = new ArrayList<>();
+    List<List<String>> result = new ArrayList<>();
     public List<List<String>> partition(String s) {
-        helper( s , 0 , new ArrayList<String>()  );
+        List<String> temp = new ArrayList<>();
         
-        return res;
+        helper(s,0,temp);
+        
+        return result;
     }
     
     public void helper( String s , int curr_index , List<String> temp ){
         if( curr_index == s.length() ){
-            res.add( new ArrayList<>(temp) );
+            result.add(new ArrayList<String>(temp));
             return;
         }
         
         for( int j = curr_index ; j<s.length();j++ ){
-            if(  isPalindrome( s , curr_index ,j )  ){
+            if( isPalindrome( s.substring( curr_index , j+1 ) ) ){
                 temp.add( s.substring(curr_index,j+1) );
-                helper( s , j+1,temp );
+                helper( s , j+1 , temp );
                 temp.remove( temp.size() - 1 );
             }
         }
-        
     }
     
-    public boolean isPalindrome( String s , int start , int end ){
+    public boolean isPalindrome( String s ){
+        int start = 0 ,end = s.length() - 1;
+        
         while( start <= end ){
             if( s.charAt(start) != s.charAt(end) ) return false;
             start++;
