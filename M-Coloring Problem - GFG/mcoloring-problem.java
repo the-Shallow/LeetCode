a458eaa32a66e794487cf3227908980ad728b1fc
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-07-05 10:01:03 +0530

    Added solution - LeetHub

--- M-Coloring Problem - GFG/mcoloring-problem.java
+++ M-Coloring Problem - GFG/mcoloring-problem.java
@@ -33,56 +33,47 @@ class solve {
     // Function to determine if graph can be coloured with at most M colours
     // such
     // that no two adjacent vertices of graph are coloured with same colour.
-    int[] colorofNode;
+    int[] colorOfNode;
     Map<Integer,List<Integer>> map = new HashMap<>();
     public boolean graphColoring(boolean graph[][], int m, int n) {
         
+        colorOfNode = new int[n];
+        
         for( int i = 0;i<graph.length;i++ ){
-            for(int j = 0;j<graph[0].length;j++){
-                // System.out.println( i + " " + j + " " + graph[i][j] );
+            List<Integer> temp = new ArrayList<>();
+            for( int j = 0;j<graph[0].length;j++ ){
                 if( graph[i][j] ){
-                    List<Integer> temp = map.getOrDefault(i,new ArrayList<>());
                     temp.add( j );
-                    
-                    map.put(i , temp);
                 }
             }
+            
+            map.put(i,temp);
         }
         
-        colorofNode = new int[n+1];
-        
-        for( int i = 0;i<=n;i++ ) colorofNode[i] = -1;
-        
-        return helper( 0 , n ,m);
-        // return false;
+        return helper(0,n,m);
         
     }
     
-    public boolean helper( int node , int n ,int m){
-        if(node == n ) return true;
+    public boolean helper( int node , int n , int m ){
+        if( node == n ) return true;
         
-        for( int j = 1;j<=m;j++ ){
-            
-            if( possible(node , j,n) ){
-                colorofNode[node] = j;
-                if( helper( node+1 , n ,m ) ) return true;
-                colorofNode[node] = -1;
+        for( int i = 1;i<=m;i++ ){
+            if( isPossible( node , i ) ){
+                colorOfNode[node] = i;
+                if( helper(node+1,n,m) ) return true;
+                colorOfNode[node] = 0;
             }
-            
         }
         
         return false;
     }
     
-    public boolean possible(int node , int color,int n){
+    public boolean isPossible(int node , int color){
+        List<Integer> temp = map.get(node);
+        if( temp == null ) return true;
         
-        List<Integer> adjacent = map.get(node);
-        
-        if(adjacent == null) return true;
-     
-        for( int val : adjacent ){
-            // System.out.println(val);
-            if( colorofNode[val] == color ) return false;
+        for( int val : temp ){
+            if( colorOfNode[val] == color ) return false;
         }
         
         return true;
