8d67925cf5d0066665ca0b959c75670f97095b5e
Shallow <87580038+the-Shallow@users.noreply.github.com>
2022-07-14 19:56:09 +0530

    Time: 9 ms (12.91%), Space: 60.2 MB (7.23%) - LeetHub

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int preIndex = 0;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(preorder,inorder);
+    }
+    
+    public  int find(int[] inorder , int val ){
+        for(int i = 0;i<inorder.length;i++){
+            if(inorder[i] == val ) return i;
+        }
+        
+        return -1;
+    }
+    
+    public TreeNode helper( int[] preorder , int[] inorder ){
+        if(inorder.length == 0) return null;
+        
+        int val = preorder[preIndex++];
+        
+        int rootIndex = find(inorder,val);
+        
+        TreeNode left = helper(preorder, Arrays.copyOfRange(inorder , 0 , rootIndex));
+        TreeNode right = helper(preorder , Arrays.copyOfRange(inorder , rootIndex + 1, inorder.length));
+        
+        TreeNode root = new TreeNode(val);
+        root.left = left;
+        root.right = right;
+        
+        return root;
+    }
+}
