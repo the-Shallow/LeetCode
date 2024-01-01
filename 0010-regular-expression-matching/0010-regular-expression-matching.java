f291cb07cb23b5a4ae3f1a440d3a685f52a07fbe
Shallow <87580038+the-Shallow@users.noreply.github.com>
2024-01-01 12:26:26 +0530

    Time: 3 ms (40.03%), Space: 42.4 MB (21.54%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,14 +1,28 @@
 class Solution {
-    Map<String,Boolean> map;
+    // Map<String,Boolean> map;
+    int[][] map;
     public boolean isMatch(String s, String p) {
-        map = new HashMap<>();
+        // map = new HashMap<>();
+        map = new int[s.length() + 1][p.length() + 1];
+        
+        for(int i = 0;i<=s.length();i++){
+            for(int j = 0;j<=p.length();j++) map[i][j] = -1;
+        }
+        
+        
         return helper(s,p,0,0);
     }
     
     public boolean helper(String s, String p, int i, int j){
         
-        String curr = i + ":" + j;
-        if(map.containsKey(curr)) return map.get(curr);
+        // String curr = i + ":" + j;
+        // if(map.containsKey(curr)) return map.get(curr);
+        
+        if( map[i][j] != -1 ){
+            return map[i][j] == 1 ? true : false;
+        }
+        
+        
         if(i >= s.length() && j>= p.length()) return true;
         
         if(j >= p.length()) return false;
@@ -17,17 +31,20 @@ class Solution {
         
         if(j+1 < p.length() && p.charAt(j+1) == '*'){
             boolean temp = helper(s,p,i,j+2) || (match && helper(s,p,i+1,j));
-            map.put(curr,temp);
+            // map.put(curr,temp);
+            map[i][j] = temp ? 1 : 0;
             return temp;
         }
         
         if( match ) {
             boolean temp = helper(s,p,i+1,j+1);
-            map.put(curr,temp);
+            // map.put(curr,temp);
+            map[i][j] = temp ? 1 : 0;
             return temp;  
         } 
         
-        map.put(curr,false);
+        // map.put(curr,false);
+        map[i][j] = 0;
         return false;
     }
 }
