2294479791d645df1cc2522e46867da84ebf714e
Shallow <87580038+the-Shallow@users.noreply.github.com>
2024-01-02 11:59:26 +0530

    Time: 6 ms (16.31%), Space: 44.7 MB (26.06%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,31 +1,18 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         Map<Integer,Integer> map = new HashMap<>();
-        
-        for(int num : nums) {
-            map.put(num, map.getOrDefault(num,0) + 1 );
-        }
-        
         List<List<Integer>> res = new ArrayList<>();
         
-        while(!map.isEmpty()){
-            List<Integer> temp = new ArrayList<>();
-            boolean flag = false;
-            for(Map.Entry<Integer,Integer> entry : map.entrySet()){
-                int key = entry.getKey();
-                int value = entry.getValue();
-                
-                // System.out.println(key + " " + value);
-                if(value == 0) continue;
-                if(!temp.contains(key)){
-                    temp.add(key);
-                    flag = true;
-                    map.put(key,value-1);
-                }
+        for(int curr : nums){
+            int count = map.getOrDefault(curr,0);
+            
+            if(res.size() == count){
+                res.add(new ArrayList<>());
             }
             
-            if(!flag) break;
-            res.add(temp);
+            res.get(count).add(curr);
+            
+            map.put(curr, count+1);
         }
         
         return res;
