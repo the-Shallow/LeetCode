8d794a8f0deef342adf7d0fb4a24f1b44d293e59
Shallow <87580038+the-Shallow@users.noreply.github.com>
2024-01-21 20:47:17 +0530

    Time: 39 ms (72.19%), Space: 49.2 MB (47.34%) - LeetHub

---
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int sumSubarrayMins(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        long res = 0;
+        
+        for(int i = 0;i<arr.length;i++){
+            
+            while(!stack.isEmpty() && arr[stack.peek()] >= arr[i]){
+                int mid = stack.pop();
+                
+                int prev_small = !stack.isEmpty() ? stack.peek() : -1;
+                
+                long count = ((mid - prev_small) * (i - mid)) % 1000000007;
+                
+                res = (res + count*arr[mid])% 1000000007;
+            }
+            
+            stack.push(i);
+        }
+        
+        while(!stack.isEmpty()){
+            int mid = stack.pop();
+                
+            int prev_small = !stack.isEmpty() ? stack.peek() : -1;
+                
+            long count = ((mid - prev_small) * (arr.length - mid))% 1000000007;
+                
+            res = (res + count*arr[mid])% 1000000007;
+        }
+        
+        return (int)(res);
+    }
+}
