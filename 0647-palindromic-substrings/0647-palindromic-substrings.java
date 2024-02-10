a33cacb635ef0e3a69989d442f06f1cb6943a4fe
Shallow <87580038+the-Shallow@users.noreply.github.com>
2024-02-10 20:20:31 +0530

    Time: 2 ms (96.29%), Space: 41.7 MB (43.63%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,33 +1,24 @@
 class Solution {
-    Map<Integer,Integer> map = new HashMap<>();
     public int countSubstrings(String s) {
         int res = 0;
+        
         for(int i = 0;i<s.length();i++){
-            res += helper(s,i);
+            res += helper(s,i,i);
+            res += helper(s,i,i+1);
         }
         
         return res;
     }
     
-    public int helper(String s, int start ){
-        // if(map.containsKey(start)) return map.get(start);
-        
+    public int helper(String s, int left, int right){
         int count = 0;
-        for(int i = start;i<s.length();i++){
-            String substringStr = s.substring(start,i+1);
-            
-            boolean flag = isPalindrome(substringStr);
-            if(flag) count++;
+        
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
+            left--;
+            right++;
         }
         
-        // map.put(start,count);
-        
         return count;
-    }
-    
-    public boolean isPalindrome(String curr){
-        StringBuilder sb = new StringBuilder(curr);
-        
-        return sb.reverse().toString().equals(curr);
     }
 }
