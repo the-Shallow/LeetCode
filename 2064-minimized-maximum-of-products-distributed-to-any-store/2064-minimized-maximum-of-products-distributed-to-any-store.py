f6f2eded2b26dbce9ead3705be4ab2f61522621d
Shallow <87580038+the-Shallow@users.noreply.github.com>
2024-11-14 23:42:41 +0530

    Time: 657 ms (44.79%), Space: 28.8 MB (59.35%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.py
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.py
@@ -1,24 +1,22 @@
 class Solution:
     def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
-        def can_distribute(amount):
-            stores = 0
-            for store in quantities:
-                stores += math.ceil( store / amount )
-                
-            return stores <= n
-                
-            
-            
         left, right = 1, max(quantities)
         result = 0
         while left <= right:
             mid = left + (right - left) // 2
             
-            if can_distribute(mid):
+            if Solution.can_distribute(mid,quantities,n):
                 result = mid
                 right = mid - 1
             else:
                 left = mid + 1
                 
         return result
+    
+    def can_distribute(amount,quantities,n):
+        stores = 0
+        for store in quantities:
+            stores += math.ceil( store / amount )
+                
+        return stores <= n
         
